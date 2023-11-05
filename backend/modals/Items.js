37c9ae9ad017160c8f445229167c3e3ef3effaf9
Ameer Hamza <ameer.hamza102002@gmail.com>
2023-11-05 17:40:03 +0500

    Create Operation Successfully Performed

--- backend/modals/Items.js
+++ backend/modals/Items.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const ItemSchema = new mongoose.Schema({
     transaction: { type: mongoose.Schema.Types.ObjectId, ref: 'Transaction', required: true },
     order: { type: mongoose.Schema.Types.ObjectId, ref: 'Order' },
-    foodItem: { type: mongoose.Schema.Types.ObjectId, ref: 'FoodItem', required: true },
+    food_item: { type: mongoose.Schema.Types.ObjectId, ref: 'FoodItem', required: true },
     quantity_sold:{type:Number, required:true}, 
   });
   
--- backend/modals/order.js
+++ backend/modals/order.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const orderSchema = new mongoose.Schema({
   user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
   restaurant: { type: mongoose.Schema.Types.ObjectId, ref: 'Restaurant', required: true },
-  totalAmount: Number,
+  totalAmount: {type:Number},
   status: { type: String, enum: ['Pending', 'Completed', 'Cancelled'], default: 'Pending' },
   total_cost:{
     type:Number
@@ -14,7 +14,9 @@ const orderSchema = new mongoose.Schema({
     default:Date.now
   },
   delivery_address:{
-    type:String
+    type:String,
+    required:true
+
   }
 });
 
